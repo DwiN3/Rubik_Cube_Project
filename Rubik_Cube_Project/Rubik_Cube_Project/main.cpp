598b8e873368fbc62673122569a9e77fdf6f4892
Narothe <110347585+Narothe@users.noreply.github.com>
2023-04-11 20:09:28 +0200

    triangle

--- Rubik_Cube_Project/Rubik_Cube_Project/main.cpp
+++ Rubik_Cube_Project/Rubik_Cube_Project/main.cpp
@@ -6,42 +6,37 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-// glfw: inicjalizacja i konfiguracja
-// ------------------------------
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec4 aColor;\n"
-"out vec4 v_Color;\n"
+"layout (location = 0) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"uniform float Position;\n"
 "void main()\n"
 "{\n"
-"v_Color = aColor;\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos.x + Position, -aPos.y, aPos.z, 1.0);\n"
+"   ourColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"in vec4 v_Color;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-//"   FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);\n"
-"FragColor = v_Color;\n"
+"   FragColor = vec4(ourColor, 1.0f);\n"
 "}\n\0";
 
 int main()
 {
-    // glfw: inicjalizacja i konfiguracja
-    // ------------------------------
+    //Konfiguracja
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // glfw - tworzenie okna
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL - P2", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -49,34 +44,18 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
-
-    // OpenGL - ustawienie aktywnego obszaru - rozmiar oraz położenie (viewport) renderowania wzgledem okna GLFW
-    // --------------------
-    // glViewport(0, 0, 800, 600);
-    // taki zapis nie zapewniw poprawnego dzialania przy przeskalowanou okna
-
-    // glfw - zarejestrowanie funkcji framebuffer_size_callback wywoływanej zwrotnie (callback function)
-    //        wywoływanej automatycznej za kazdym razem gdy wielkosc okna ulegnie zmianie
-    //        uaktualniajacej viewport
-    // --------------------
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // glad: zaladowanie wszystkich funkcji (wskaznikow) OpenGL
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-
-    // Utworzenie i kompilacja shaderow
-    // ------------------------------------
     // vertex shader
     int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
-    // sprawdzenie bledow
     int success;
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -86,12 +65,10 @@ int main()
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-
     // fragment shader
     int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
-    // sprawdzenie bledow
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success)
     {
@@ -99,13 +76,11 @@ int main()
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-
     // linkowanie
     int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
-    // sprawdzenie bledow
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
@@ -115,106 +90,76 @@ int main()
     glDeleteShader(fragmentShader);
 
 
-    // Wprowadzenie danych wierzcholkow i ustawienie atrybitow
-    // ------------------------------------------------------------------
+    // Program
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, /**/ 0.9f, 0.5f, 0.4f, 1.0f, // lewy 0
-         0.5f, -0.5f, 0.0f, /**/ 0.5f, 0.6f, 0.6f, 1.0f, // prawy 1
-         0.0f,  0.5f, 0.0f, /**/ 0.9f, 0.3f, 0.4f, 1.0f, // górny 2
+        // pozycja           // kolor
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // dolny prawy
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // dolny lewy
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // górny
 
-         -0.5f / 2, 0.0f,  0.0f, /**/ 0.9f, 0.3f, 0.4f, 1.0f, // lewy środek 3
-         0.0f,      -0.5f, 0.0f, /**/ 0.4f, 0.4f, 0.2f, 1.0f, // dolny środek 4
-         0.5f / 2,  0.0f,  0.0f, /**/ 0.5f, 0.9f, 0.1f, 1.0f  //prawy środek 5
     };
 
-    int indices[] = {
-        4,3,0,
-        4,1,5
-    };
-
-    // Utworzenie vertex buffer objects (VBO) oraz Vertex Array Object (VAO)
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
-    //Najpierw podpięcie Vertex Array Object, później podpięcie i ustawienie atrybutów vertex buffer(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
+    // atrybuty pozycji
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
 
+    // atrybuty koloru
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (const void*)12);
 
-    // Odpięcie
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // glBindVertexArray(0);  // mamy tylko jedno VAO wiec nie musimy go aktywowac / dezaktywowac - caly czas aktywne
 
-    // Odpiecie VAO
-    glBindVertexArray(0);
+    // poniewaz mamy tylko jeden program shadera mozemy go aktywowac przed petla renderowania
+    glUseProgram(shaderProgram);
 
 
-    // rysowanie wireframe
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // uniform
+    float v = 0.5f;
+    float loc = glGetUniformLocation(shaderProgram, "Position");
 
-    // petla renderowania - bez niej program po wyrysowaniu pojedynczego obrazu zakonczyl by dzialanie i zamknal okno
-    // -----------
+    glUseProgram(shaderProgram);
+    glUniform1f(loc, v);
+
+
+    // petla renderowania
     while (!glfwWindowShouldClose(window))
     {
-        // przetwazanie wejscia przy renderowaniu kazdej klatki
-        // kazda iteracja petli renderowania nosi nazwe klatki
-        // -----
         processInput(window);
-
-        // komendy renderowania pojedynczej klatki - pojedynczy przebieg petli renderowania
-        // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // rysowanie trójkąta
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // mamy tylko jedno VAO, wiec w zasadzie nie trzeba za kazdym razem podpinac
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0); // nie trzeba za kazdym razem odpinav
+        // renderowanie trojkata
+        // glBindVertexArray(VAO); // mamy tylko jedno VAO wiec nie musimy go aktywowac / dezaktywowac - caly czas aktywne
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // glfw: zamiana bufora ekranu oraz przeglsadniecie puli zdazen IO (wcisniete klawisze, ruch myszy)
-        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Zwolnienie pamięci
-    // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
-    // glfw: zakonczenie dzialania, czyszczenie i zwalnianie zaalokowanych zasobow GLFW
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
-// obsluga wejscia: odpytywanie GLFW czy jakikolwiek klawisz zostal wcisniety/puszczony i reakcja na to.
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: gdy wielosc okna ulega zmianie ta funkcja jest automatycznie wywoływana
-// width oraz height - to nowe wymiary okna GLFW - automatycznie wypelniane
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // ustawienie poprawnego viewport po przeskalowaniu okna
     glViewport(0, 0, width, height);
 }
